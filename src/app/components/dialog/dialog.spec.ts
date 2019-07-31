8409a589fc08f70b3a3d5bea8a7d6169be1d3937
Rafael Fakhrutdinov <rfakhrutdinov@fix.ru>
2019-07-31 11:06:05 +0300

    upd ver

--- src/app/components/dialog/dialog.spec.ts
+++ src/app/components/dialog/dialog.spec.ts
@@ -4,74 +4,88 @@ import { Dialog } from './dialog';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('Dialog', () => {
-    
+
     let dialog: Dialog;
     let fixture: ComponentFixture<Dialog>;
-    
+
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [
-                NoopAnimationsModule    
+                NoopAnimationsModule
             ],
             declarations: [
                 Dialog
             ]
         });
-        
+
         fixture = TestBed.createComponent(Dialog);
         dialog = fixture.componentInstance;
     });
-    
+
     it('should display the header', () => {
-        dialog.visible=true;
         dialog.header = 'PrimeNG Dialog Header';
         fixture.detectChanges();
         const headerEl = fixture.debugElement.query(By.css('.ui-dialog-title'));
         expect(headerEl.nativeElement.textContent).toContain('PrimeNG Dialog Header')
     });
-    
-    it('should display the close icon when closable', () => {
-        dialog.visible=true;
+
+    it('should display close icon when closable', () => {
         fixture.detectChanges();
         const closeEl = fixture.debugElement.query(By.css('.ui-dialog-titlebar-close'));
         expect(closeEl).not.toBeNull();
     });
-    
-    it('should display the resizer when resizable is true', () => {
-        dialog.visible=true;
+
+    it('should display resizer when resizable', () => {
         fixture.detectChanges();
         const resizeEl = fixture.debugElement.query(By.css('.ui-resizable-handle'));
         expect(resizeEl).not.toBeNull();
     });
 
-    it('should not create the container element by default', () => {
+    it('should be hidden by default', () => {
         fixture.detectChanges();
-        expect(fixture.debugElement.nativeElement.childElementCount).toEqual(0);
-        expect(dialog.visible).toEqual(undefined);
+        expect(fixture.debugElement.children[0].styles.display).toEqual('none');
     });
-    
+
     it('should add rtl class when rtl is enabled', () => {
-        dialog.visible=true;
         dialog.rtl = true;
         fixture.detectChanges();
         expect(fixture.debugElement.children[0].classes['ui-dialog-rtl']).toEqual(true);
     });
-    
+
     it('should add draggable class when dragging is enabled', () => {
-        dialog.visible=true;
         fixture.detectChanges();
         expect(fixture.debugElement.children[0].classes['ui-dialog-draggable']).toEqual(true);
     });
-            
+
+    it('should show the dialog when visible is true', () => {
+        spyOn(dialog, 'show');
+        dialog.visible = true;
+        fixture.detectChanges();
+        expect(fixture.debugElement.children[0].styles.display).toEqual('block');
+        expect(dialog.show).toHaveBeenCalled();
+    });
+
+    it('should call hide if visible is true and dialog gets hidden', () => {
+        dialog.visible = true;
+        fixture.detectChanges();
+
+        spyOn(dialog, 'hide');
+        dialog.visible = false;
+        fixture.detectChanges();
+
+        expect(fixture.debugElement.children[0].styles.display).toEqual('none');
+        expect(dialog.hide).toHaveBeenCalled();
+    });
+
     it('should update visible as false binding when close icon is clicked', () => {
         let show = true;
         dialog.visible = show;
         fixture.detectChanges();
         dialog.visibleChange.subscribe(value => show = value);
-        
+
         const closeEl = fixture.nativeElement.querySelector('.ui-dialog-titlebar-close');
         closeEl.click();
-        
+
         expect(show).toEqual(false);
     });
 
